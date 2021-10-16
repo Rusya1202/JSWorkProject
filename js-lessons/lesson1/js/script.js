13a1dcc59d4372aaac7f86f84de1204c5ebeae1a
Rusya <ruslanming1202@gmail.com>
2021-10-16 19:02:49 +0300

    Commit_3--lesson14

--- js-lessons/lesson1/js/script.js
+++ js-lessons/lesson1/js/script.js
@@ -1,43 +1,29 @@
 "use strict";
 
-const num = 50;
+let num = 50;
 
-if (num < 50) {
-    console.log('Больше!');
-} else if (num > 99) {
-    console.log('Много!');
-} else {
-    console.log ('Пойдёт)');
+while (num <= 100) {
+    console.log(num);
+    num++;
 }
 
-const numTwo = 50;
+let numTwo = 25;
 
-(numTwo === 50) ? console.log ('Пойдёт)') : console.log ('Неееее)');
+do {
 
-const numThree = 49;
-
-switch (numThree) {
-    case 40 :
-        console.log('Добавь ещё 10');
-        break;
-
-    case 60 :
-        console.log('Убавь 10 и всё будет ок');
-        break;
-
-    case 51 :
-        console.log('Почтиииииии');
-        break;
-
-    case 49 :
-        console.log('Почтиииииии');
-        break;
-
-    case 50 : 
-        console.log('Ай маладэс!');
-        break;
+    console.log(numTwo);
+    numTwo++;
     
-    default :
-        console.log('Чёт ты не то написал :/');
-        break;
+} while (numTwo <= 100);
+
+let numThree = 30;
+
+for (let i = 1; i < 50; i++) {
+
+    if (i === 30) {
+        continue;
+    }
+
+    console.log(i);
+    numThree++;
 }
