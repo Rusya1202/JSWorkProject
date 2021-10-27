7a7d6733e84508543d45051af5137408ad79067a
Rusya <ruslanming1202@gmail.com>
2021-10-27 10:38:10 +0300

    Commit_8--lesson23

--- js-lessons/lesson1/js/script.js
+++ js-lessons/lesson1/js/script.js
@@ -1,80 +1,19 @@
 "use strict"; 
 
-let obj = {
-    a: 12,
-    b: 15,
-    c: {
-        d: 500,
-        e: 240
-    }
+const human = {
+    height: 170,
+    weight: 65,
+    sayHello: function () {
+        console.log('Hello');
+    },
+    health: 100
 };
 
-function copy(someObj) {
-    let objCopy = {};
+const marry = Object.create(human);
 
-    let key;
-    for (key in someObj) {
-        objCopy[key] = someObj[key];
-    };
+//console.log(marry.health);
 
-    return objCopy;
-};
-
-console.log(obj);
-
-//let objClone = copy(obj);
-
-//objClone.a = 256;
-
-//console.log(objClone);
-
-//let clone = Object.assign({}, obj);
-
-//clone.a = 525;
-
-//console.log(clone);
-
-
-
-
-
-
-
-
-
-let array = ['a', 'b', 'c'];
-
-console.log(array);
-
-//let newArray = array.slice();
-
-//newArray[2] = 0;
-
-//console.log(newArray);
-
-let arrayClone = [...array];
-
-arrayClone[2] = 0;
-
-console.log(arrayClone);
-
-
-
-
-
-
-
-
-
-function log(a, b, c) {
-    console.log(a);
-    console.log(b);
-    console.log(c);
-};
-
-const num = [1, 3, 5];
-
-log(...num);
+marry.sayHello();
 
 
 
