a544ee598c55cb0332de122fbbbcf046a15e354c
Rusya <ruslanming1202@gmail.com>
2021-10-28 20:07:52 +0300

    Commit_10--lesson26

--- js-lessons/lesson1/js/script.js
+++ js-lessons/lesson1/js/script.js
@@ -1,5 +1,37 @@
 "use strict"; 
 
+//To string 
+
+//1)
+console.log(typeof(String(null)));
+
+//2)
+console.log(typeof(null + ''));
 
 
+
+//To number
+
+//1)
+console.log(typeof(Number('5')));
+
+//2)
+console.log(typeof(+'57'));
+
+//3) 
+console.log(typeof(parseInt("15px", 10)));
+
+
+
+//To boolean
+ 
+//1)
+//0, '', null, undefined, NaN === false (отсутствие значений)
+//(Есть значение) === true
+
+//2) 
+console.log(typeof(Boolean('4')));
+
+//3) 
+console.log(typeof(!!"444"));
 
