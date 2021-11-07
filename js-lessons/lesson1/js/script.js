f10315ff6ac15c29e5ca8af66b3b8e90f4bf8bac
Rusya <ruslanming1202@gmail.com>
2021-11-07 13:00:18 +0300

    Commit_15--lesson28

--- js-lessons/lesson1/js/script.js
+++ js-lessons/lesson1/js/script.js
@@ -1,67 +1,31 @@
 "use strict";
 
-//let x = 5;
-//alert(x++);
-//Выведет 5 тк инеремент в постфиксной форме
+// const firstBox = document.getElementById('nav-link');
+// console.log(firstBox);
+// Возвращает элемент с данным id
 
 
-
-//[] + false + null + true;
-//Выведет значение NaN 
-
-
-
-//let y = 1;
-//let x = y = 2;
-//alert(x);
-//Выведет 2
+// const secondBox = document.getElementsByTagName('body');
+// console.log(secondBox);
+// Возвращает "HTML-Коллекцию"(псевдо-массив) с элементами в данных тегах
 
 
-
-//[] + 1 + 2;
-//Выведет 12 тк [] при конкатинации выдаёт пустую строку ""
-
-
-
-//alert("1"[0]);
-//Выведет 1 тк в строке "1" нулевым элементом является 1
+// const thirdBox = document.getElementByClassName('header');
+// console.log(thirdBox);
+// Возвращает "HTML-Коллекцию"(псевдо-массив) с элементами с данным классом
 
 
-
-//2 && 1 && null && 0 && undefined;
-//Оператор && "И" запинается на лжи (в этом случае на null)
-//Оператор || "ИЛИ" запинается на правде 
+// ==============================================================================================
 
 
-
-//console.log(!!(a && b) === (a && b));
-//Выведет false тк !! превращает в булийное 
-
-
-
-// alert(null || 2 && 3 || 4);
-// Выведет 3 тк && приоритетнее чем ||
-// PS смотри таблицу приоритетов операторов JS
+// const fourthBox = document.querySelectorAll('.header');
+// console.log(fourthBox);
+// Возвращает "HTML-Коллекцию"(псевдо-массив с методом "forEach") с элементами с данным селектором (CSS) 
+// .header.forEach(item => {
+//     console.log(item)
+// });
 
 
-
-// const a = [1, 2, 3];
-// const b = [1, 2, 3];
-// a == b;
-//Массив a не равен массиву b тк это разные элементы с похожими данными 
-
-
-
-// alert(+'infinity');
-// Выведет infinity но в числовом типе 
-
-
-
-// colsole.log("Ёжик" > "яблоко");
-// Смотри приоритеты букв 
-
-
-
-// console.log(0 || "" || 2 || undefined || true || false);
-// Выведет 2 тк "Оператор || "ИЛИ" запинается на правде"
-
+// const fifthBox = document.querySelector('#header');
+// console.log(fifthBox);
+// Возвращает элемент с данным селектором (CSS)
