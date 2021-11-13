292e351e69e844d198f0bb94a329177e515140b9
Rusya <ruslanming1202@gmail.com>
2021-11-13 13:33:38 +0300

    Commit_16--lesson29

--- js-lessons/lesson1/js/script.js
+++ js-lessons/lesson1/js/script.js
@@ -1,5 +1,9 @@
 "use strict";
 
+// ==============================================================================================
+//Получение элементов
+// ==============================================================================================
+
 // const firstBox = document.getElementById('nav-link');
 // console.log(firstBox);
 // Возвращает элемент с данным id
@@ -17,7 +21,6 @@
 
 // ==============================================================================================
 
-
 // const fourthBox = document.querySelectorAll('.header');
 // console.log(fourthBox);
 // Возвращает "HTML-Коллекцию"(псевдо-массив с методом "forEach") с элементами с данным селектором (CSS) 
@@ -29,3 +32,66 @@
 // const fifthBox = document.querySelector('#header');
 // console.log(fifthBox);
 // Возвращает элемент с данным селектором (CSS)
+
+// ==============================================================================================
+//Действия с элементами
+// ==============================================================================================
+
+const   firstBox = document.getElementById('nav-link'),
+        secondBox = document.getElementsByTagName('body'),
+        thirdBox = document.getElementByClassName('header'),
+        superBox = document.querySelector('.container'),
+        fourthBox = container.querySelectorAll('.header'),
+        fifthBox = container.querySelector('.header');
+
+
+// firstBox.style.backgroundColor = 'blue';
+// firstBox.style.width = '50px';
+//ИЛИ
+// firstBox.style.cssText = `background-color: #fff; width: 500px; height: auto;`;
+// secondBox[0].style.width = '500px';
+
+
+
+// for (let i = 0; i < fourthBox.lenght; i++) {
+//     fourthBox[i].style.backgroundColor = 'black';
+// }
+
+// fourthBox.forEach( item => {
+//     item.style.backgroundColor = 'black';
+// });
+
+// ==============================================================================================
+
+const div = document.createElement('div');
+const text = document.createTextNode('Hello World!');
+
+div.classList.add('container');
+
+// fourthBox.append(div);
+// fourthBox.appendChild(div); -- Старый метод
+
+// fourthBox.pretend(div);
+
+// fourthBox[1].before(div);
+// fourthBox[1].after(div);
+// container.incertBefore(div, fourthBox[1]); -- Старый метод
+
+// fourthBox[0].remove();
+// container.removeChild(fourthBox[0]); -- Старый метод
+
+// fourthBox[0].replaceWith(secondBox[1]);
+// container.replaceChild(fourthBox[0], secondBox[1]); -- Старый метод
+
+
+// div.innerHTML = '<h1>Hello world!</h1>';
+// div.textContent = '<h1>Hello world!</h1>';
+
+// div.insertAdjacentHTML('beforebegin/afterbegin/beforeend/afterend', '<h1>Hello world!</h1>'); 
+
+
+
+
+
+
+
